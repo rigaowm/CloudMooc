4b1e242b3b2d3fa812e75559aa6f7dc56f6b81d8
rigaowm <760268966@qq.com>
2023-12-16 15:46:07 +0800

    初始化仓库

---
+++ xuecheng-plus-parent/src/main/java/com/xuecheng/XuechengPlusParentApplication.java
@@ -0,0 +1,14 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusParentApplication {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+        SpringApplication.run(XuechengPlusParentApplication.class, args);
+    }
+
+}
