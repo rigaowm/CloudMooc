1c61e444189800fd4fe588b96667584920c7d394
rigaowm <760268966@qq.com>
2023-12-16 15:12:34 +0800

    初始化

---
+++ xuecheng-plus-base/src/test/java/com/xuecheng/xuechengplusbase/XuechengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xuechengplusbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
