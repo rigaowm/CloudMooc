3ef8f7f7f43e2424ea091457391a7695ff937dbb
rigaowm <760268966@qq.com>
2023-12-20 19:46:40 +0800

    dev02

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,8 +4,10 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
 @RestController //相当于Controller+ResponseBody
 public class CourseBaseInfoController {
+    @Autowired
+    private CourseBaseInfoService courseBaseInfoService;
 
     @ApiOperation("课程信息分页查询接口")
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
-
-        return null;
+        return courseBaseInfoService.queryCourseBaseList(pageParams,queryCourseParamsDto);
     }
 
 }
